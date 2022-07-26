c5a9024321edc479859e3f4e5e090f9a0b71a4af
Konrad Miekus <miekus.konrad@gmail.com>
2022-07-26 07:41:53 +0200

    fetch data from strapi cms

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 import Head from 'next/head'
+import { getProducts } from '../lib/products'
 
-const Home = () => {
+export const getStaticProps = async () => {
+  const products = await getProducts()
+  return {
+    props: { products },
+    revalidate: 5 * 60,
+  }
+}
+
+const HomePage = ({ products }) => {
   return (
    <>
      <Head>
        <title>Next shop</title>
      </Head>
-     <main>
-       <h1>Next shop</h1>
+     <main className='p-10'>
+       <h1 className='text-lg'>Next shop</h1>
+       <ul>
+         {products.map(product => (
+           <li key={product.id}>
+             {product.title}
+           </li>
+         ))}
+       </ul>
      </main>
    </>
   )
 }
 
-export default Home
+export default HomePage
